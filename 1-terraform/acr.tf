ee2a900a6a11ceb9e870e66e7888ed20aff0ebbb
Nicolas Yuen <niyuen@microsoft.com>
2019-08-20 14:46:32 +0200

    checking resource group variable and instead referencing the RG object to avoid dependency issues

--- 1-terraform/acr.tf
+++ 1-terraform/acr.tf
@@ -1,6 +1,6 @@
 resource "azurerm_container_registry" "acr" {
   name                     = var.acr_name
-  resource_group_name      = var.resource_group
+  resource_group_name      = azurerm_resource_group.aks_demo_rg.name
   location                 = var.azure_region
   sku                      = "Basic"
   admin_enabled            = false
